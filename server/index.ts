885fabc93ba514d20e73739984912f3601a632a3
srwa-rsp <srwa.rashidpoor.1993@gmail.com>
2025-03-17 20:00:57 -0400

    add auth route

--- server/index.ts
+++ server/index.ts
@@ -3,6 +3,8 @@ import dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./config/database";
 import storyRoutes from "./routes/story";
+import authRoutes from "./routes/auth";
+
 
 dotenv.config();
 connectDB(); 
@@ -11,7 +13,8 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.use("/auth", authRoutes); 
 app.use("/stories", storyRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(` Server running on port ${PORT}`));
